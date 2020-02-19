77ca533e5701dddfe3240f7012bdfe53524c51f3
EntityParrot_ <entityparrot@qq.com>
2020-02-19 09:10:03 +0800

    Update delete Method.

--- src/main/java/org/serverct/parrot/parrotx/data/PData.java
+++ src/main/java/org/serverct/parrot/parrotx/data/PData.java
@@ -1,6 +1,18 @@
 package org.serverct.parrot.parrotx.data;
 
+import org.serverct.parrot.parrotx.PPlugin;
 import org.serverct.parrot.parrotx.flags.Uniqued;
+import org.serverct.parrot.parrotx.utils.LocaleUtil;
 
 public interface PData extends PConfiguration, Uniqued {
+
+    @Override
+    default void delete() {
+        PPlugin plugin = getID().getPlugin();
+        if (getFile().delete()) {
+            plugin.getLang().logAction(LocaleUtil.DELETE, getTypeName());
+        } else {
+            plugin.getLang().logError(LocaleUtil.DELETE, getTypeName(), "无法删除该文件");
+        }
+    }
 }
