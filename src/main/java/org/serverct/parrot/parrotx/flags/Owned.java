ef58cf6110b9be12cc47bb4ca54282b3a810addf
EntityParrot_ <entityparrot@qq.com>
2020-02-19 13:27:45 +0800

    Add a default method for Flag-Owned.

--- src/main/java/org/serverct/parrot/parrotx/flags/Owned.java
+++ src/main/java/org/serverct/parrot/parrotx/flags/Owned.java
@@ -3,7 +3,9 @@ package org.serverct.parrot.parrotx.flags;
 public interface Owned {
     String getOwner();
 
-    boolean isOwner(String name);
+    default boolean isOwner(String name) {
+        return getOwner().equals(name);
+    }
 
     boolean setOwner(String name);
 }
