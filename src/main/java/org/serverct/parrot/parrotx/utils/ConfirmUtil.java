8c7ca7a1458a6c3617a987fd0fbf48e030fa0b04
EntityParrot_ <entityparrot@qq.com>
2020-03-31 09:01:02 +0800

    Update

--- src/main/java/org/serverct/parrot/parrotx/utils/ConfirmUtil.java
+++ src/main/java/org/serverct/parrot/parrotx/utils/ConfirmUtil.java
@@ -1,6 +1,7 @@
 package org.serverct.parrot.parrotx.utils;
 
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 import org.serverct.parrot.parrotx.ParrotX;
 
 import java.util.HashMap;
@@ -9,21 +10,27 @@ import java.util.UUID;
 
 public class ConfirmUtil {
     private static Map<String, Map<UUID, Boolean>> confirmMap = new HashMap<>();
+    private static Map<String, Map<UUID, BukkitTask>> confirmTask = new HashMap<>();
 
     public static boolean confirm(String key, UUID user, int interval) {
         Map<UUID, Boolean> targetConfirm = confirmMap.getOrDefault(key, new HashMap<>());
+        Map<UUID, BukkitTask> targetTask = confirmTask.getOrDefault(key, new HashMap<>());
         boolean result = targetConfirm.getOrDefault(user, false);
+        BukkitTask task = targetTask.getOrDefault(user, null);
         if (!result) {
             targetConfirm.put(user, true);
             confirmMap.put(key, targetConfirm);
 
-            new BukkitRunnable() {
+            if (task != null) task.cancel();
+            task = new BukkitRunnable() {
                 @Override
                 public void run() {
                     targetConfirm.put(user, false);
                     confirmMap.put(key, targetConfirm);
                 }
             }.runTaskLater(ParrotX.getPlugin(ParrotX.class), interval * 20);
+            targetTask.put(user, task);
+            confirmTask.put(key, targetTask);
         }
         return result;
     }
