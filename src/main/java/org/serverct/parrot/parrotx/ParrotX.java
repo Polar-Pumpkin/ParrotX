2f9131cf7de179a2b217b80b92174057aa9ab685
EntityParrot_ <entityparrot@qq.com>
2020-07-02 22:58:11 +0800

    Update

--- src/main/java/org/serverct/parrot/parrotx/ParrotX.java
+++ src/main/java/org/serverct/parrot/parrotx/ParrotX.java
@@ -1,7 +1,12 @@
 package org.serverct.parrot.parrotx;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.serverct.parrot.parrotx.listener.InventoryClickListener;
 
 public final class ParrotX extends JavaPlugin {
-
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(new InventoryClickListener(), this);
+    }
 }
