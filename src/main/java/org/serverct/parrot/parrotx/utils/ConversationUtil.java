f074df4c2929cb6bd71e5a436c924c1178f19bf0
EntityParrot_ <entityparrot@qq.com>
2020-08-30 23:08:11 +0800

    [+] ConversationUtil 新增了带超时后的提示信息的方法重载
    本次 Commit 共包含以下内容:
    - [+] 为 ConversationUtil 提供了带超时后的提示信息的 start 方法重载.

--- src/main/java/org/serverct/parrot/parrotx/utils/ConversationUtil.java
+++ src/main/java/org/serverct/parrot/parrotx/utils/ConversationUtil.java
@@ -8,28 +8,35 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.serverct.parrot.parrotx.PPlugin;
 
+import java.util.Objects;
+
 public class ConversationUtil {
-    public static void start(@NonNull PPlugin plugin, @NonNull Player user, Prompt firstPrompt, int timedOut) {
+    public static void start(@NonNull PPlugin plugin, @NonNull Player user, Prompt firstPrompt, int timedOut, String message) {
         BasicUtil.closeInventory(plugin, user);
         Conversation conversation = new ConversationFactory(plugin)
                 .withFirstPrompt(firstPrompt)
                 .withLocalEcho(false)
                 .buildConversation(user);
         conversation.begin();
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                conversation.abandon();
-            }
-        }.runTaskLater(plugin, timedOut * 20);
+
+        if (timedOut > 0) {
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    conversation.abandon();
+                    if (Objects.nonNull(message)) {
+                        I18n.send(user, I18n.color(message));
+                    }
+                }
+            }.runTaskLater(plugin, timedOut * 20);
+        }
     }
 
     public static void start(@NonNull PPlugin plugin, @NonNull Player user, Prompt firstPrompt) {
-        BasicUtil.closeInventory(plugin, user);
-        new ConversationFactory(plugin)
-                .withFirstPrompt(firstPrompt)
-                .withLocalEcho(false)
-                .buildConversation(user)
-                .begin();
+        start(plugin, user, firstPrompt, -1, null);
+    }
+
+    public static void start(@NonNull PPlugin plugin, @NonNull Player user, Prompt firstPrompt, int timedOut) {
+        start(plugin, user, firstPrompt, timedOut, null);
     }
 }
