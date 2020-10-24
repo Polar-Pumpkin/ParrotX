d49b81d75664c7ff9df45a882298a49239a53c32
EntityParrot_ <entityparrot@qq.com>
2020-10-24 19:10:10 +0800

    [+] BaseElement 新增 of 方法.
    本次 Commit 共包含以下内容:
    - 新增 BaseElement#of 方法用于快速从 ConfigurationSection 数据中构建 Gui 元素.

--- src/main/java/org/serverct/parrot/parrotx/data/inventory/element/BaseElement.java
+++ src/main/java/org/serverct/parrot/parrotx/data/inventory/element/BaseElement.java
@@ -2,13 +2,18 @@ package org.serverct.parrot.parrotx.data.inventory.element;
 
 import lombok.Builder;
 import lombok.Data;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+import org.serverct.parrot.parrotx.PPlugin;
 import org.serverct.parrot.parrotx.data.inventory.BaseInventory;
 import org.serverct.parrot.parrotx.data.inventory.InventoryElement;
 import org.serverct.parrot.parrotx.enums.Position;
+import org.serverct.parrot.parrotx.utils.BasicUtil;
+import org.serverct.parrot.parrotx.utils.ItemUtil;
+import org.serverct.parrot.parrotx.utils.i18n.I18n;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +30,25 @@ class BaseElement implements InventoryElement {
     private final String xPos;
     private final String yPos;
     private final Predicate<Player> condition;
+
+    public static BaseElement of(final PPlugin plugin, final ConfigurationSection section, final int priority, final Predicate<Player> condition) {
+        final String name = section.getName();
+        final Supplier<ItemStack> item = () -> ItemUtil.build(plugin, section);
+        final ConfigurationSection posSection = section.getConfigurationSection("Position");
+        if (BasicUtil.isNull(plugin, posSection, I18n.LOAD, "Gui 元素 " + name, "未找到 Position 数据节")) {
+            return null;
+        }
+        final String x = posSection.getString("X", "0");
+        final String y = posSection.getString("Y", "0");
+        return BaseElement.builder()
+                .priority(priority)
+                .name(name)
+                .item(item)
+                .xPos(x)
+                .yPos(y)
+                .condition(condition)
+                .build();
+    }
 
     public boolean condition(final Player user) {
         return Objects.isNull(condition) || condition.test(user);
