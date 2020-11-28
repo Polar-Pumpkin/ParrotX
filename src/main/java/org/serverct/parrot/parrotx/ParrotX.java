02b316d582aa3806734b37f9a81bc79a9191359c
EntityParrot_ <entityparrot@qq.com>
2020-11-28 10:56:09 +0800

    [*] 优化 Inventory 监听
    本次 Commit 共包含以下内容:
    - [*] 为 InventoryListener 加上了所属插件验证, 避免出现多个监听器导致 Inventory 的点击事件被重复处理的情况.

--- src/main/java/org/serverct/parrot/parrotx/ParrotX.java
+++ src/main/java/org/serverct/parrot/parrotx/ParrotX.java
@@ -1,12 +1,6 @@
 package org.serverct.parrot.parrotx;
 
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.serverct.parrot.parrotx.listener.InventoryListener;
 
 public final class ParrotX extends JavaPlugin {
-    @Override
-    public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(new InventoryListener(), this);
-    }
 }
--- src/main/java/org/serverct/parrot/parrotx/listener/InventoryListener.java
+++ src/main/java/org/serverct/parrot/parrotx/listener/InventoryListener.java
@@ -1,5 +1,6 @@
 package org.serverct.parrot.parrotx.listener;
 
+import lombok.NonNull;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -7,32 +8,62 @@ import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.InventoryHolder;
+import org.serverct.parrot.parrotx.PPlugin;
 import org.serverct.parrot.parrotx.data.inventory.InventoryExecutor;
 
 public class InventoryListener implements Listener {
+
+    private final PPlugin plugin;
+
+    public InventoryListener(final @NonNull PPlugin plugin) {
+        this.plugin = plugin;
+    }
+
     @EventHandler
     public void onClick(InventoryClickEvent event) {
         InventoryHolder holder = event.getInventory().getHolder();
-        if (holder != null) if (holder instanceof InventoryExecutor) ((InventoryExecutor) holder).execute(event);
+        if (holder instanceof InventoryExecutor) {
+            final InventoryExecutor executor = (InventoryExecutor) holder;
+            if (plugin.equals(executor.getPlugin())) {
+                executor.execute(event);
+            }
+        }
     }
 
     @EventHandler
     public void onClose(InventoryCloseEvent event) {
         InventoryHolder holder = event.getInventory().getHolder();
-        if (holder != null) if (holder instanceof InventoryExecutor) ((InventoryExecutor) holder).close(event);
+        if (holder instanceof InventoryExecutor) {
+            final InventoryExecutor executor = (InventoryExecutor) holder;
+            if (plugin.equals(executor.getPlugin())) {
+                executor.close(event);
+            }
+        }
     }
 
     @EventHandler
     public void onOpen(InventoryOpenEvent event) {
         InventoryHolder holder = event.getInventory().getHolder();
-        if (holder != null) if (holder instanceof InventoryExecutor) ((InventoryExecutor) holder).open(event);
+        if (holder instanceof InventoryExecutor) {
+            final InventoryExecutor executor = (InventoryExecutor) holder;
+            if (plugin.equals(executor.getPlugin())) {
+                executor.open(event);
+            }
+        }
     }
 
     @EventHandler
     public void onEvent(InventoryEvent event) {
-        if (event instanceof InventoryCloseEvent || event instanceof InventoryClickEvent || event instanceof InventoryOpenEvent)
+        if (event instanceof InventoryCloseEvent
+                || event instanceof InventoryClickEvent
+                || event instanceof InventoryOpenEvent)
             return;
         InventoryHolder holder = event.getInventory().getHolder();
-        if (holder != null) if (holder instanceof InventoryExecutor) ((InventoryExecutor) holder).event(event);
+        if (holder instanceof InventoryExecutor) {
+            final InventoryExecutor executor = (InventoryExecutor) holder;
+            if (plugin.equals(executor.getPlugin())) {
+                executor.event(event);
+            }
+        }
     }
 }
