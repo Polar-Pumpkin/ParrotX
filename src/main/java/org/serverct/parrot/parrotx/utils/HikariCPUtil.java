1d3b08759a2fd7071f585058e9ca94c35c2fc1c3
EntityParrot_ <entityparrot@qq.com>
2021-02-05 23:50:28 +0800

    [*] 优化 HikariCPUtil
    本次 Commit 共包含以下内容:
    - [*] java.lang.NullPointerException;

--- src/main/java/org/serverct/parrot/parrotx/utils/HikariCPUtil.java
+++ src/main/java/org/serverct/parrot/parrotx/utils/HikariCPUtil.java
@@ -2,17 +2,25 @@ package org.serverct.parrot.parrotx.utils;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.serverct.parrot.parrotx.PPlugin;
 import org.serverct.parrot.parrotx.utils.i18n.I18n;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class HikariCPUtil {
 
-    private static HikariDataSource sqlConnectionPool;
+    private static final Map<PPlugin, HikariDataSource> sqlConnectionPool = new HashMap<>();
 
-    public static void setSqlConnectionPool(final String address, final String port, final String database,
+    public static void setSqlConnectionPool(final PPlugin plugin,
+                                            final String address, final String port, final String database,
                                             final String username, final String password) {
         final HikariConfig config = new HikariConfig();
         config.setDriverClassName("com.mysql.jdbc.Driver");
@@ -29,16 +37,88 @@ public class HikariCPUtil {
 
         config.setAutoCommit(true);
 
-        sqlConnectionPool = new HikariDataSource(config);
+        sqlConnectionPool.put(plugin, new HikariDataSource(config));
     }
 
-    public Connection getConnection(final PPlugin plugin) {
+    public static Connection getConnection(final PPlugin plugin) {
         try {
-            return sqlConnectionPool.getConnection();
+            final HikariDataSource source = sqlConnectionPool.get(plugin);
+            if (Objects.isNull(source)) {
+                plugin.getLang().log.error(I18n.GET, "数据库链接", "未初始化 SQL 链接池.");
+                return null;
+            }
+            return source.getConnection();
         } catch (SQLException e) {
             plugin.getLang().log.error(I18n.GET, "数据库链接", e, plugin.getPackageName());
         }
         return null;
+    }
+
+    public static void execute(@NotNull final PPlugin plugin, @NotNull final String sql,
+                               @Nullable final String... args) {
+        final Connection connection = getConnection(plugin);
+        if (Objects.isNull(connection)) {
+            return;
+        }
+
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(sql);
+            if (Objects.nonNull(args) && args.length > 0) {
+                for (int index = 0; index < args.length; index++) {
+                    statement.setString(index + 1, args[index]);
+                }
+            }
+            statement.execute();
+        } catch (SQLException exception) {
+            plugin.getLang().log.error(I18n.EXECUTE, "MySQL 语句", exception, plugin.getPackageName());
+        } finally {
+            close(plugin, connection, statement, null);
+        }
+    }
+
+    @Nullable
+    public static ResultSet query(@NotNull final PPlugin plugin, @NotNull final String sql,
+                                  @Nullable final String... args) {
+        final Connection connection = getConnection(plugin);
+        if (Objects.isNull(connection)) {
+            return null;
+        }
+
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(sql);
+            if (Objects.nonNull(args) && args.length > 0) {
+                for (int index = 0; index < args.length; index++) {
+                    statement.setString(index + 1, args[index]);
+                }
+            }
+            return statement.executeQuery();
+        } catch (SQLException exception) {
+            plugin.getLang().log.error(I18n.EXECUTE, "MySQL 语句", exception, plugin.getPackageName());
+        } finally {
+            close(plugin, connection, statement, null);
+        }
+        return null;
+    }
+
+    public static void close(@NotNull final PPlugin plugin,
+                             @Nullable final Connection connection,
+                             @Nullable final PreparedStatement statement,
+                             @Nullable final ResultSet result) {
+        try {
+            if (Objects.nonNull(connection)) {
+                connection.close();
+            }
+            if (Objects.nonNull(statement)) {
+                statement.close();
+            }
+            if (Objects.nonNull(result)) {
+                result.close();
+            }
+        } catch (final SQLException e) {
+            plugin.getLang().log.error("关闭", "数据库链接", e, "serverct");
+        }
     }
 
 }
