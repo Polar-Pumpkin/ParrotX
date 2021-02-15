bf91e84f61a0969d10403b61d535f1be08730ec0
EntityParrot_ <entityparrot@qq.com>
2021-02-15 14:30:33 +0800

    [*] 优化 InventoryPlaceholder.
    本次 Commit 共包含以下内容:
    - [*] :3

--- src/main/java/org/serverct/parrot/parrotx/data/inventory/element/InventoryPlaceholder.java
+++ src/main/java/org/serverct/parrot/parrotx/data/inventory/element/InventoryPlaceholder.java
@@ -8,14 +8,13 @@ import org.bukkit.Material;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.serverct.parrot.parrotx.data.inventory.InventoryElement;
 import org.serverct.parrot.parrotx.data.inventory.PInventory;
 import org.serverct.parrot.parrotx.utils.i18n.I18n;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -46,6 +45,34 @@ public class InventoryPlaceholder implements InventoryElement {
 
     public ItemStack getPlaced(final int slot) {
         return this.placedMap.get(slot);
+    }
+
+    @NotNull
+    public List<ItemStack> addPlaced(final ItemStack... items) {
+        final List<ItemStack> result = new ArrayList<>();
+        if (Objects.isNull(items) || items.length <= 0) {
+            return result;
+        }
+
+        final Iterator<ItemStack> iterator = Arrays.asList(items).iterator();
+        for (int slot : getPositions()) {
+            if (!iterator.hasNext()) {
+                break;
+            }
+            if (this.placedMap.containsKey(slot)) {
+                continue;
+            }
+            this.placedMap.put(slot, iterator.next().clone());
+        }
+
+        while (iterator.hasNext()) {
+            result.add(iterator.next().clone());
+        }
+        return result;
+    }
+
+    public void clear() {
+        this.placedMap.clear();
     }
 
     @Override
