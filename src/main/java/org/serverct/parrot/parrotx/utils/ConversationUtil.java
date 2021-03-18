59b05ea970ee9adb8e75def9c41e9ec51bf8e97c
EntityParrot_ <entityparrot@qq.com>
2021-03-18 14:24:30 +0800

    [*] ConversationUtil 新增回调函数.
    本次 Commit 共包含以下内容:
    - [*] 在操作过期时可以执行自定义代码.
    - [*] 在对话已结束时不会触发超时操作.

--- src/main/java/org/serverct/parrot/parrotx/utils/ConversationUtil.java
+++ src/main/java/org/serverct/parrot/parrotx/utils/ConversationUtil.java
@@ -1,22 +1,23 @@
 package org.serverct.parrot.parrotx.utils;
 
 import lombok.NonNull;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.conversations.Conversation;
 import org.bukkit.conversations.ConversationFactory;
 import org.bukkit.conversations.Prompt;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.serverct.parrot.parrotx.PPlugin;
 import org.serverct.parrot.parrotx.utils.i18n.I18n;
 
-import java.util.Objects;
-
 public class ConversationUtil {
-    public static void start(@NotNull PPlugin plugin, @NotNull Player user, Prompt firstPrompt, int timedOut,
-                             String message) {
+    public static void start(@NotNull final PPlugin plugin, @NotNull final Player user,
+                             @Nullable final Prompt firstPrompt, final int timedOut,
+                             @Nullable final String message, @Nullable final Runnable onTimedOut) {
         InventoryUtil.closeInventory(plugin, user);
-        Conversation conversation = new ConversationFactory(plugin)
+        final Conversation conversation = new ConversationFactory(plugin)
                 .withFirstPrompt(firstPrompt)
                 .withLocalEcho(false)
                 .buildConversation(user);
@@ -26,20 +27,24 @@ public class ConversationUtil {
             new BukkitRunnable() {
                 @Override
                 public void run() {
+                    if (conversation.getState() == Conversation.ConversationState.ABANDONED) {
+                        return;
+                    }
                     conversation.abandon();
-                    if (Objects.nonNull(message)) {
+                    if (!StringUtils.isEmpty(message)) {
                         I18n.send(user, I18n.color(message));
                     }
+                    BasicUtil.canDo(onTimedOut, Runnable::run);
                 }
             }.runTaskLater(plugin, timedOut * 20L);
         }
     }
 
     public static void start(@NonNull PPlugin plugin, @NonNull Player user, Prompt firstPrompt) {
-        start(plugin, user, firstPrompt, -1, null);
+        start(plugin, user, firstPrompt, -1, null, null);
     }
 
     public static void start(@NonNull PPlugin plugin, @NonNull Player user, Prompt firstPrompt, int timedOut) {
-        start(plugin, user, firstPrompt, timedOut, null);
+        start(plugin, user, firstPrompt, timedOut, null, null);
     }
 }
